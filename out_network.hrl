50e79ff8587c116ef5fd823845fd339c59783b2f
Simon Kiener <jugglinghobo@gmail.com>
2017-12-06 08:47:42 +0100

    added all project files

---
+++ out_network.hrl
@@ -0,0 +1,24 @@
+2017-12-5 17:31 | creating random network
+<6679.38.0>: online
+<6679.38.0>: linked to <6681.38.0>
+<6679.38.0>: linked to <6682.38.0>
+<6680.38.0>: online
+<6682.38.0>: online
+<6682.38.0>: linked to <6679.38.0>
+<6681.38.0>: online
+<6680.38.0>: linked to <6682.38.0>
+<6682.38.0>: linked to <6681.38.0>
+<6682.38.0>: linked to <6680.38.0>
+<6681.38.0>: linked to <6680.38.0>
+<6680.38.0>: linked to <6681.38.0>
+<6681.38.0>: linked to <6682.38.0>
+<6681.38.0>: linked to <6679.38.0>
+<6679.38.0>: client dani (<6756.60.0>) connected
+<6679.38.0>: client test (<6759.60.0>) connected
+<6679.38.0>: client test (<6759.60.0>) connected
+<6679.38.0>: client test (<6759.60.0>) connected
+<6679.38.0>: client test (<6759.60.0>) connected
+<6680.38.0>: client radi (<6758.60.0>) connected
+<6680.38.0>: client radi (<6758.60.0>) connected
+<6680.38.0>: client radi (<6758.60.0>) connected
+<6680.38.0>: client <6758.60.0> disconnected
